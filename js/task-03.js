f9db188bc92bafbf0e8d91f51a4104be04afdf12
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-04-24 18:33:07 +0300

    Добавляет решение с append

---
+++ js/task-03.js
@@ -0,0 +1,40 @@
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryEl = document.querySelector('#gallery');
+
+const makeElement = (images) => {
+
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('image');
+  
+    const imagesEl = document.createElement('img');
+    imagesEl.src = images.url;
+    imagesEl.alt = images.alt;
+    imagesEl.width = '240';
+    imagesEl.classList.add('img');
+  
+    itemEl.appendChild(imagesEl);
+    return itemEl;
+};
+
+const element = images.map(makeElement);
+
+
+galleryEl.append(...element);
+// galleryEl.insertAdjacentHTML(afterbegin, ...element)
