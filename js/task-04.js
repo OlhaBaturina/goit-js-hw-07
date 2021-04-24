bc32812666fa3183ba8482076dea722403e4369b
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-04-25 01:45:31 +0300

    убирает консольлог

--- js/task-04.js
+++ js/task-04.js
@@ -5,9 +5,6 @@ value: document.querySelector('#value'),
 increment: document.querySelector('[data-action="increment"]'),
 decrement: document.querySelector('[data-action="decrement"]'),
 };
-console.log(refs.decrement);
-console.log(refs.increment);
-console.log(refs.value);
 
 refs.decrement.addEventListener('click', onDecrement);
 refs.increment.addEventListener('click', onIncrement);
