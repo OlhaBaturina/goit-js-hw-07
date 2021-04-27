7a1fa3b23e2b184235dba3b5e3e8664f3199d33b
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-04-27 21:57:44 +0300

    вносит правки в 6 задание

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,16 @@
 const refs = {
 input: document.querySelector('#validation-input'),
 valid: document.querySelector('.valid'),
-invalid: document.querySelector('.invalid'),
+    invalid: document.querySelector('.invalid'),
 };
+console.dir(refs.input)
 
 refs.input.addEventListener('blur', onInputValue);
 
 function onInputValue(event) {
     event.currentTarget.classList.add('invalid');
-    if (event.currentTarget.value.length === 6) {
+    event.currentTarget.classList.remove('valid');
+    if (event.currentTarget.value.length === Number(refs.input.dataset.length)) {
         event.currentTarget.classList.remove('invalid');
         event.currentTarget.classList.add('valid');
     }
